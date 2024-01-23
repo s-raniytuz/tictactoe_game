bfd5d8c7144aa108d2c33b0e5bb02cbe15d9fef7
s-raniytuz <tuzsocial909@gmail.com>
2024-01-22 20:11:17 -0700

    v0.02

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,7 @@ gameBoard.addEventListener('click', function (evt) {
     const placeItem = currentItem.textContent;
     if (target.textContent === '') {
         target.textContent = placeItem;
+        target.classList.add('transition-class');
         currentSwitch();
         checkGameStatus();
     }
@@ -38,17 +39,21 @@ function checkGameStatus() {
         gameItems[0].style.color = 'red';
     }*/
     const row1 = [gameItems[0], gameItems[1], gameItems[2]];
-    const row2 = [];
-    const row3 = [];
-    const column1 = [];
-    const column2 = [];
-    const column3 = [];
-    const diagonalLeftRight = [];
-    const diagoanalRightLEft = [];
+    const row2 = [gameItems[3], gameItems[4], gameItems[5]];
+    const row3 = [gameItems[6], gameItems[7], gameItems[8]];
+    const column1 = [gameItems[0], gameItems[3], gameItems[6]];
+    const column2 = [gameItems[1], gameItems[4], gameItems[7]];
+    const column3 = [gameItems[2], gameItems[5], gameItems[8]];
+    const diagonalLeftRight = [gameItems[0], gameItems[4], gameItems[8]];
+    const diagoanalRightLEft = [gameItems[6], gameItems[4], gameItems[2]];
 
     if (allEqual(row1)) {
         console.log('it works');
     } else if (allEqual(row2)) {
+        console.log('it works');
+    } else if (allEqual(row3)) {
+        console.log('it works');
+    } else if (allEqual(column1)) {
         console.log('it works');
     }
 }
