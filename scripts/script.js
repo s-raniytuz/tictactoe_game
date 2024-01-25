6d3ff73c583fe8c31dc3267cd9ccde97d09f2e36
s-raniytuz <tuzsocial909@gmail.com>
2024-01-24 20:30:01 -0700

    v0.1

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,22 @@ const crossLine = document.querySelector('#cross-line');
 
 let counterX = 0;
 let counterO = 0;
+let clickCounter = 0;
 
 function allEqual(arr) {
     if (arr[0].textContent !== '') {
         return arr.every((item) => item.textContent === arr[0].textContent);
+    }
+}
+
+function clickEvent(evt) {
+    const target = evt.target;
+    const placeItem = currentItem.textContent;
+    if (target.textContent === '') {
+        target.textContent = placeItem;
+        target.classList.add('transition-class');
+        currentSwitch();
+        checkGameStatus();
     }
 }
 
@@ -42,20 +54,46 @@ function checkGameStatus() {
     const diagonalLeftRight = [gameItems[0], gameItems[4], gameItems[8]];
     const diagonalRightLeft = [gameItems[6], gameItems[4], gameItems[2]];
 
+    clickCounter++;
+
     if (allEqual(row1)) {
         itemTransform(row1, row2.concat(row3));
         crossLine.classList.remove('hidden');
         crossLine.classList.add('winRow1');
+        gameBoard.removeEventListener('click', clickEvent);
+        return true;
     } else if (allEqual(row2)) {
         itemTransform(row2, row1.concat(row3));
+        crossLine.classList.remove('hidden');
+        crossLine.classList.add('winRow2');
+        gameBoard.removeEventListener('click', clickEvent);
+        return true;
     } else if (allEqual(row3)) {
         itemTransform(row3, row1.concat(row2));
+        crossLine.classList.remove('hidden');
+        crossLine.classList.add('winRow3');
+        gameBoard.removeEventListener('click', clickEvent);
+        return true;
     } else if (allEqual(column1)) {
         itemTransform(column1, column2.concat(column3));
+        crossLine.classList.remove('hidden');
+        crossLine.parentElement.classList.add('rotate');
+        crossLine.parentElement.classList.add('winColumn1');
+        gameBoard.removeEventListener('click', clickEvent);
+        return true;
     } else if (allEqual(column2)) {
         itemTransform(column2, column1.concat(column3));
+        crossLine.classList.remove('hidden');
+        crossLine.parentElement.classList.add('rotate');
+        crossLine.parentElement.classList.add('winColumn2');
+        gameBoard.removeEventListener('click', clickEvent);
+        return true;
     } else if (allEqual(column3)) {
         itemTransform(column3, column1.concat(column2));
+        crossLine.classList.remove('hidden');
+        crossLine.parentElement.classList.add('rotate');
+        crossLine.parentElement.classList.add('winColumn3');
+        gameBoard.removeEventListener('click', clickEvent);
     } else if (allEqual(diagonalLeftRight)) {
         const arrLost = [];
         gameItems.forEach((item) => {
@@ -64,6 +102,10 @@ function checkGameStatus() {
             }
         });
         itemTransform(diagonalLeftRight, arrLost);
+        crossLine.classList.remove('hidden');
+        crossLine.parentElement.classList.add('rotate45');
+        crossLine.parentElement.classList.add('winDiagonal');
+        gameBoard.removeEventListener('click', clickEvent);
     } else if (allEqual(diagonalRightLeft)) {
         const arrLost = [];
         gameItems.forEach((item) => {
@@ -72,16 +114,15 @@ function checkGameStatus() {
             }
         });
         itemTransform(diagonalRightLeft, arrLost);
+        crossLine.classList.remove('hidden');
+        crossLine.parentElement.classList.add('rotate135');
+        crossLine.parentElement.classList.add('winDiagonal');
+        gameBoard.removeEventListener('click', clickEvent);
+    } else {
+        if (clickCounter === 9) {
+            gameItems.forEach((item) => item.classList.add('lose-class'));
+        }
     }
 }
 
-gameBoard.addEventListener('click', function (evt) {
-    const target = evt.target;
-    const placeItem = currentItem.textContent;
-    if (target.textContent === '') {
-        target.textContent = placeItem;
-        target.classList.add('transition-class');
-        currentSwitch();
-        checkGameStatus();
-    }
-});
+gameBoard.addEventListener('click', clickEvent);
